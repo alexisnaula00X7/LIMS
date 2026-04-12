ec096fe88309358bca3b45d979defe01c8132b90
alexisnaula00X7 <117377553+alexisnaula00X7@users.noreply.github.com>
2026-04-12 04:18:05 -0500

    Change Supabase URL and key in lims.py
    Updated Supabase connection credentials in lims.py.

--- lims.py
+++ lims.py
@@ -6,8 +6,8 @@ from datetime import date
 # --- 1. CONFIGURACIÓN DE CONEXIÓN ---
 # Se usan etiquetas. Los valores reales van en el panel "Secrets" de Streamlit Cloud.
 try:
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
+    url = st.secrets["https://pfdthsxlhpncheutmunm.supabase.co"]
+    key = st.secrets["sb_publishable_jv-VDpWieP9D0r0tUEndmw_ZYQB0NJz"]
     supabase: Client = create_client(url, key)
 except Exception as e:
     st.error("⚠️ Error: No se encontraron las credenciales en los Secrets de Streamlit.")
